66b5b6d65be921aa2f090323f0ccc079f063fd9e
Reem <janinareem@gmail.com>
2021-12-14 00:28:59 +0200

    Update index.js file

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const menu = document.querySelector('.mobile-menu-container');
+const menu = document.querySelector('#menu-container');
 const closeButton = document.querySelector('.close-button');
 const hamburguer = document.querySelector('#hamburguer-button');
 const anchor = document.querySelector('.mobile-menu-ul li a');
 
 hamburguer.addEventListener('click', function (event) {
   console.log('The button was clicked!');
+  menu.classList.add('visible');
 });
+
+
+closeButton.addEventListener('click',function (event){
+  menu.classList.remove('visible');
+});
+
+
