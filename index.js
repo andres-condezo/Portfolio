0f48a4511b3e433c6bb62f204b503ad990886d5a
andres-condezo <ra.condezomonge@gmail.com>
2021-12-13 16:38:54 -0500

    add mobile menu function

---
+++ index.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.mobile-menu-container');
+const closeButton = document.querySelector('.close-button');
+const hamburguer = document.querySelector('#hamburguer-button');
+const anchor = document.querySelector('.mobile-menu-ul li a');
+
+hamburguer.addEventListener('click', function (event) {
+  console.log('The button was clicked!');
+});
