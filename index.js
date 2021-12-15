3a5dd74abb8fbaa3e0a9df1ef82a3ac1ffe2db4a
andres-condezo <ra.condezomonge@gmail.com>
2021-12-15 14:27:02 -0500

    Update modal function

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
-function main () {
-  const menu = document.querySelector('#menu-container');
-  const closeButton = document.querySelector('.close-button');
-  const hamburguer = document.querySelector('#hamburguer-button');
-  const body = document.querySelector('body');
-  const anchors = document.querySelectorAll('.mobile-menu-ul li');
+// mobile menu
 
-  // mobile menu
+const menu = document.querySelector('#menu-container');
+const closeButton = document.querySelector('.close-button');
+const hamburguer = document.querySelector('#hamburguer-button');
+const body = document.querySelector('body');
+const anchors = document.querySelectorAll('.mobile-menu-ul li');
 
-  function openMenu () {
-    menu.classList.add('visible');
-    body.classList.add('overflow-hidden');
-  }
-
-  function closeMenu () {
-    menu.classList.remove('visible');
-    body.classList.remove('overflow-hidden');
-  }
-
-  hamburguer.addEventListener('click', openMenu);
-  closeButton.addEventListener('click', closeMenu);
-  anchors.forEach((link) => {
-    link.addEventListener('click', closeMenu);
-  });
+function openMenu () {
+  menu.classList.add('visible');
+  body.classList.add('overflow-hidden');
 }
 
-// html elements
+function closeMenu () {
+  menu.classList.remove('visible');
+  body.classList.remove('overflow-hidden');
+}
+
+hamburguer.addEventListener('click', openMenu);
+closeButton.addEventListener('click', closeMenu);
+anchors.forEach((link) => {
+  link.addEventListener('click', closeMenu);
+});
+
+// works-section
 
 const data = [
   {
-    name: 'Project title 1',
+    name: 'Tonic',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    image: './img/Snapshoot-Portfolio.png',
+    image: './img/Snapshoot-Portfolio1.png',
     technologies: ['html', 'css', 'javaScript'],
-    liveVersion: 'https://andres-condezo.github.io/Portfolio/',
-    source: 'https://github.com/andres-condezo/Portfolio'
+    liveVersion: '#',
+    source: '#',
+    popup: '0'
   },
   {
-    name: 'Project title 2',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    image: './img/Snapshoot-Portfolio.png',
-    technologies: ['html', 'css', 'javaScript'],
-    liveVersion: 'https://andres-condezo.github.io/Portfolio/',
-    source: 'https://github.com/andres-condezo/Portfolio'
+    name: 'Multi-Post Stories',
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    image: './img/Snapshoot-Portfolio2.png',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    liveVersion: '#',
+    source: '#',
+    popup: '1'
   },
   {
-    name: 'Project title 3',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    image: './img/Snapshoot-Portfolio.png',
-    technologies: ['html', 'css', 'javaScript'],
-    liveVersion: 'https://andres-condezo.github.io/Portfolio/',
-    source: 'https://github.com/andres-condezo/Portfolio'
+    name: 'Facebook 360',
+    description: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    image: './img/Snapshoot-Portfolio3.png',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    liveVersion: '#',
+    source: '#',
+    popup: '2'
+  },
+  {
+    name: 'Uber Navigation',
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    image: './img/Snapshoot-Portfolio4.png',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    liveVersion: '#',
+    source: '#',
+    popup: '3'
   }
 ];
-
-//
 
 const my_projects = data.map((el, index) => {
   const article = document.createElement('article');
@@ -85,7 +93,7 @@ const my_projects = data.map((el, index) => {
 					</ul>
 					<a href="#works__card2" class="btn works__action" id="showModal">See Project</a>
 				</section>
-`;
+  `;
   return article;
 });
 
@@ -98,7 +106,7 @@ for (let i = 0; i < my_projects.length; i += 1) {
 // modal section
 
 const closeButton2 = document.querySelector('#close-button-2');
-const modalBtn = document.querySelector('#showModal');
+const modalBtn = document.querySelectorAll('#showModal');
 const modal = document.querySelector('#modal-container');
 
 function openModal () {
@@ -111,120 +119,85 @@ function closeModal () {
   body.classList.remove('overflow-hidden');
 }
 
-modalBtn.addEventListener('click', openModal);
+modalBtn.forEach((link) => {
+  link.addEventListener('click', openModal);
+});
 closeButton2.addEventListener('click', closeModal);
 
-// cards
-/*
-			<article class="works__card">
-				<figure class="snapshoot">
-					<img src="./img/Snapshoot-Portfolio.png" alt="">
-				</figure>
-				<section class="works__left-block">
-					<div class="works__primary-text">
-						<h3 class="works__project-title">Tonic</h3>
-						<div class="frame-2">
-							<span class="client">CANOPY</span>
-							<div class="counter"></div>
-							<span class="role">Back End Dev</span>
-							<div class="counter"></div>
-							<span class="year">2015</span>
-						</div>
-					</div>
-					<p class="works__primary-par">
-						A daily selection of privately personalized reads; no accounts or sign-ups required.
-					</p>
-					<ul class="works__tags">
-						<li>html</li>
-						<li>css</li>
-						<li>javaScript</li>
-					</ul>
-					<a href="#works__card2" class="btn works__action">See Project</a>
-				</section>
-			</article>
+const dataModal = [
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: './img/Snapshoot-Portfolio1.png',
+    technologies: ['html', 'css', 'javaScript'],
+    liveVersion: '#',
+    source: '#',
+    popup: '0'
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    image: './img/Snapshoot-Portfolio2.png',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    liveVersion: '#',
+    source: '#',
+    popup: '1'
+  },
+  {
+    name: 'Facebook 360',
+    description: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    image: './img/Snapshoot-Portfolio3.png',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    liveVersion: '#',
+    source: '#',
+    popup: '2'
+  },
+  {
+    name: 'Uber Navigation',
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    image: './img/Snapshoot-Portfolio4.png',
+    technologies: ['html', 'Ruby on rails', 'css', 'javaScript'],
+    liveVersion: '#',
+    source: '#',
+    popup: '3'
+  }
+];
 
-			<article class="works__card card2">
-				<figure class="snapshoot">
-					<img src="./img/Snapshoot-Portfolio2.png" alt="">
-				</figure>
-				<section class="works__left-block block2">
-					<div class="works__primary-text">
-						<h3 class="works__project-title">Multi-Post Stories</h3>
-						<div class="frame-2">
-							<span class="client">FACEBOOK</span>
-							<div class="counter"></div>
-							<span class="role">Full Stack Dev</span>
-							<div class="counter"></div>
-							<span class="year">2015</span>
-						</div>
-					</div>
-					<p class="works__primary-par">
-						Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.
-					</p>
-					<ul class="works__tags">
-						<li>html</li>
-						<li>Ruby on rails</li>
-						<li>css</li>
-						<li>javaScript</li>
-					</ul>
-					<a href="#works__card" class="btn works__action">See Project</a>
-				</section>
-			</article>
+const myModalProjects = dataModal.map((el, index) => {
+  const modalCard = document.createElement('article');
+  modalCard.className = 'modal-card';
+  modalCard.innerHTML = `
+    <button class="close-button-2" id="close-button-2">x</button>
+    <section class="works__left-block">
+      <div class="works__primary-text">
+        <h3 class="modal__project-title">${el.name}</h3>
+        <div class="frame-2">
+          <span class="client">CANOPY</span>
+          <div class="counter"></div>
+          <span class="role">Back End Dev</span>
+          <div class="counter"></div>
+          <span class="year">2015</span>
+        </div>
+      </div>
+      <figure class="snapshoot-modal">
+        <img src="./img/Snapshoot-Portfolio2.png" alt="">
+      </figure>
+      <p class="works__primary-par">Description</p>
+      <ul class="works__tags">
+        <li>html</li>
+        <li>css</li>
+        <li>JS</li>
+      </ul>
+      <a href="#works__card2" class="btn works__action">See Project</a>
+    </section>
+  `;
+  return modalCard;
+});
 
-			<article class="works__card">
-				<figure class="snapshoot">
-					<img src="./img/Snapshoot-Portfolio3.png" alt="">
-				</figure>
-				<section class="works__left-block">
-					<div class="works__primary-text">
-						<h3 class="works__project-title">Facebook 360</h3>
-						<div class="frame-2">
-							<span class="client">FACEBOOK</span>
-							<div class="counter"></div>
-							<span class="role">Full Stack Dev</span>
-							<div class="counter"></div>
-							<span class="year">2015</span>
-						</div>
-					</div>
-					<p class="works__primary-par">
-						Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.
-					</p>
-					<ul class="works__tags">
-						<li>html</li>
-						<li>Ruby on rails</li>
-						<li>css</li>
-						<li>javaScript</li>
-					</ul>
-					<a href="#works__card2" class="btn works__action">See Project</a>
-				</section>
-			</article>
+const modalSection = document.querySelector('#modal-container');
+for (let j = 0; j < myModalProjects.length; j += 1) {
+  modalSection.appendChild(myModalProjects[j]);
+}
 
-			<article class="works__card card2">
-				<figure class="snapshoot">
-					<img src="./img/Snapshoot-Portfolio4.png" alt="">
-				</figure>
-				<section class="works__left-block block2">
-					<div class="works__primary-text">
-						<h3 class="works__project-title">Uber Navigation</h3>
-						<div class="frame-2">
-							<span class="client">UBER</span>
-							<div class="counter"></div>
-							<span class="role">Lead Developer</span>
-							<div class="counter"></div>
-							<span class="year">2018</span>
-						</div>
-					</div>
-					<p class="works__primary-par">
-						A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.
-					</p>
-					<ul class="works__tags">
-						<li>html</li>
-						<li>css</li>
-						<li>javaScript</li>
-					</ul>
-					<a href="#works__card" class="btn works__action">See Project</a>
-				</section>
-			</article>
-      */
-
-main();
+{ /* const myDiv = document.createElement('LI'); */ }
+{ /* modalSection.appendChild(myDiv); */ }
