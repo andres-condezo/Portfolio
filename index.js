c1e3db45249d73b78cf7162f5617226c162b8c12
andres-condezo <ra.condezomonge@gmail.com>
2021-12-16 14:32:42 -0500

    Fix regular expresion

--- index.js
+++ index.js
@@ -202,14 +202,17 @@ window.onscroll = function () { myFunction(); };
 
 const form = document.querySelector('#form');
 const email = document.querySelector('#email');
-const emailInput = email.value;
 const msg = document.querySelector('#errorMsg');
-const regex = /[a-z]/;
+const regex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/;
 
 form.addEventListener('submit', (event) => {
+  const emailInput = email.value;
   if (!regex.test(emailInput)) {
     event.preventDefault();
     msg.classList.add('showError');
     msg.textContent = '*The content of the email field has to be in lower case.';
+  } else {
+    msg.classList.remove('showError');
+    msg.innerHTML = '';
   }
 });
