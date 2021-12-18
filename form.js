ac51bc1ade8abd92c2bdf2585ba667b77ee8e0dc
andres-condezo <ra.condezomonge@gmail.com>
2021-12-18 13:32:26 -0500

    rename localstorage file to form

--- form.js
+++ form.js
@@ -1,13 +1,52 @@
-const formStorage = document.querySelector('#form');
-const nameInput = formStorage.querySelector('#name');
-const emailInput = formStorage.querySelector('#email');
-const msgInput = formStorage.querySelector('#msg');
+//* **************
+// form section
+//* **************
+
+const form = document.querySelector('#form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msgInput = document.querySelector('#errorMsg');
+const errorMsg = '* The content of the email field has to be in lower case.';
+const regex = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/;
+
+//* **************
+// form validation
+//* **************
+
+function isValidEmail (emailValue) {
+  return regex.test(emailValue);
+}
+
+function showError () {
+  msg.classList.add('showError');
+  msg.textContent = errorMsg;
+}
+
+function deleteErrorMessage () {
+  msg.classList.remove('showError');
+  msg.innerHTML = '';
+}
+
+form.addEventListener('submit', (event) => {
+  const emailValue = email.value;
+
+  if (!isvalid(emailValue)) {
+    event.preventDefault();
+    showError();
+  } else {
+    deleteErrorMessage();
+  }
+});
+
+//* *****************
+// form local storage
+//* *****************
 
 const saveData = () => {
   const formData = {
     name: nameInput.value,
     email: emailInput.value,
-    msg: msgInput.value,
+    msg: msgInput.value
   };
   localStorage.setItem('formData', JSON.stringify(formData));
 };
